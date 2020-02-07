2810724d39630af85f73d4592c043ac95dc57399
Andrew Hoefling <andrewh@fileonq.com>
2020-02-07 10:07:04 -0500

    Initial commit

---
+++ src/EMDKConfigUpdates/BarcodeScannerManager.cs
@@ -0,0 +1,109 @@
+﻿using Android.Content;
+using Symbol.XamarinEMDK;
+using Symbol.XamarinEMDK.Barcode;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace EMDKConfigUpdates
+{
+	public class BarcodeScannerManager : Java.Lang.Object, EMDKManager.IEMDKListener
+	{
+		private EMDKManager _emdkManager;
+		private BarcodeManager _barcodeManager;
+		private Scanner _scanner;
+		public event EventHandler<Scanner.DataEventArgs> ScanReceived;
+		public bool IsScannerEnabled { get; private set; } = false;
+		public BarcodeScannerManager(Context context)
+		{
+			InitializeEMDK(context);
+		}
+
+		void InitializeEMDK(Context context)
+		{
+			var results = EMDKManager.GetEMDKManager(context, this);
+			if (results.StatusCode != EMDKResults.STATUS_CODE.Success)
+				throw new InvalidOperationException("Unable to initialize EMDK Manager");
+		}
+
+		void InitializeBarcodeManager()
+		{
+			_barcodeManager = (BarcodeManager)_emdkManager?.GetInstance(EMDKManager.FEATURE_TYPE.Barcode);
+			if (_barcodeManager == null)
+				return;
+
+			if (_barcodeManager.SupportedDevicesInfo?.Count > 0)
+				_scanner = _barcodeManager.GetDevice(BarcodeManager.DeviceIdentifier.Default);
+		}
+
+		public void OnClosed()
+		{
+			if (_scanner != null)
+			{
+				_scanner.Data -= OnScanReceived;
+				_scanner.Status -= OnStatusChanged;
+
+				_scanner.Disable();
+				_scanner.Release();
+				_scanner = null;
+			}
+
+			if (_emdkManager != null)
+			{
+				_emdkManager.Release();
+				_emdkManager = null;
+			}
+		}
+
+		public void OnOpened(EMDKManager manager)
+		{
+			_emdkManager = manager;
+
+			// We can only initialize the barcode manager once we retrieve
+			// the EMDK Manager, which is invoked by via callback in the
+			// EMDK library
+			InitializeBarcodeManager();
+		}
+
+		public void EnableScanner()
+		{
+			if (_scanner == null)
+				return;
+
+			_scanner.Data += OnScanReceived;
+			_scanner.Status += OnStatusChanged;
+
+			_scanner.Enable();
+			_scanner.TriggerType = Scanner.TriggerTypes.Hard;
+
+			IsScannerEnabled = true;
+		}
+
+		public void DisableScanner()
+		{
+			if (_scanner == null)
+				return;
+
+			_scanner.Data -= OnScanReceived;
+			_scanner.Status -= OnStatusChanged;
+
+			_scanner.Disable();
+
+			IsScannerEnabled = false;
+		}
+
+		private void OnStatusChanged(object sender, Scanner.StatusEventArgs args)
+		{
+			if (args?.P0?.State == StatusData.ScannerStates.Idle)
+			{
+				Task.Delay(100);
+				_scanner.Read();
+			}
+		}
+
+		private void OnScanReceived(object sender, Scanner.DataEventArgs args)
+		{
+			MainThread.BeginInvokeOnMainThread(() => ScanReceived?.Invoke(sender, args));
+		}
+	}
+}
---
+++ src/EMDKConfigUpdates/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Symbol.XamarinEMDK.Barcode;
+
+namespace EMDKConfigUpdates
+{
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity, View.IOnClickListener
+	{
+		private BarcodeScannerManager _scannerManager;
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+			_scannerManager = new BarcodeScannerManager(this);
+			_scannerManager.ScanReceived += OnScanReceived;
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.activity_main);
+
+			var toggleScannerButton = FindViewById<Button>(Resource.Id.toggle_scanner_button);
+			toggleScannerButton.SetOnClickListener(this);
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+		{
+			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+
+			_scannerManager.ScanReceived -= OnScanReceived;
+			_scannerManager.Dispose();
+			_scannerManager = null;
+		}
+
+		private void OnScanReceived(object sender, Scanner.DataEventArgs args)
+		{
+			var scanDataCollection = args.P0;
+			if (scanDataCollection?.Result == ScannerResults.Success)
+			{
+				var textView = FindViewById<TextView>(Resource.Id.last_scan_received);
+				if (textView != null)
+					textView.Text = scanDataCollection.GetScanData()[0].Data;
+			}
+		}
+
+		public void OnClick(View v)
+		{
+			if (_scannerManager.IsScannerEnabled)
+				_scannerManager.DisableScanner();
+			else
+				_scannerManager.EnableScanner();
+		}
+	}
+}
